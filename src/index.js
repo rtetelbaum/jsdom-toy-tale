25e91a84df2c46f06fe10e6bbbf02b58f4ed263f
jomariepolanco <jomariepolanco@gmail.com>
2020-11-20 10:24:03 -0500

    add new toy

--- src/index.js
+++ src/index.js
@@ -2,8 +2,21 @@ let addToy = false;
 
 document.addEventListener("DOMContentLoaded", () => {
   const addBtn = document.querySelector("#new-toy-btn");
-	const toyFormContainer = document.querySelector(".container");
-	const toyCollection = document.querySelector("#toy-collection")
+  const toyFormContainer = document.querySelector(".container");
+  const toyCollection = document.querySelector("#toy-collection");
+  const toyForm = document.querySelector(".add-toy-form")
+
+
+  toyForm.addEventListener("submit", event => {
+	event.preventDefault()
+	const toyData = {
+		name: event.target.name.value,
+		image: event.target.image.value,
+		likes: 0
+	}
+	event.target.reset()
+	toyDataFetchPost(toyData)
+  })
 
   addBtn.addEventListener("click", () => {
     // hide & seek with the form
@@ -21,6 +34,19 @@ document.addEventListener("DOMContentLoaded", () => {
 		 .then(toysData => {
 			 console.log(toysData)
 			 renderAllToys(toysData)
+		 })
+	}
+
+	const toyDataFetchPost = (toyData) => {
+		return fetch("http://localhost:3000/toys", {
+			method: "POST",
+			headers: {"Content-Type": "application/json"},
+			body: JSON.stringify(toyData)
+		})
+		 .then(response => response.json())
+		 .then(newToyData => {
+			 console.log('Success:', newToyData)
+			 renderOneToy(newToyData)
 		 })
 	}
 
